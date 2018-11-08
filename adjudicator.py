55981e7cf19cc98bdc4cc1aff7d0a0348ce40a8c
James Morris <jamesamorris@yahoo.com>
2018-11-08 16:50:03 -0500

    Add diceroll, add gameFinished basic, add beginnings of play

--- adjudicator.py
+++ adjudicator.py
@@ -1,6 +1,32 @@
+import random
+
 class Adjudicator(object):
 	def __init__(self, playerModel1, playerModel2):
 		self.players = (playerModel1, playerModel2)
-		#TODO Initialize game state
+		
+		# Set some constants
+		self.maxTurns = 100
+
+		#Initialize game state
+		self.turn = 0		
+
+
+	def diceRoll(self):
+		# Get two random ints to make it closer to the regular dist.
+		die1 = random.randint(0,5)
+		die2 = random.randint(0,5)
+		return die1 + die2
+
+	def gameFinished(self):
+		# Increase the number of turns
+		self.turn = self.turn+1
+		return self.turn >= self.maxTurns
+
+	# Plays the game, returns a csv
+	def play(self):
+		while not self.gameFinished():
+			roll = self.diceRoll()
+			
+		
 	
 		
