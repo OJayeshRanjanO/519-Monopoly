1b28ae0656f123daebceb06c4b553034c0d4bd37
James Morris <jamesamorris@yahoo.com>
2018-11-13 16:28:05 -0500

    Add functionality to load cards. Currently loads dummy cards

--- library.py
+++ library.py
@@ -1,15 +1,32 @@
 import json
 
-def loadGameConfigurations(location):
+def loadGameJson(location):
 	gameJson = None
 	with open(location, "r") as fp:
 		gameJson = json.load(fp)
+	return gameJson
 
+def loadGameProperties(gameJson):
 	groups = gameJson["groups"]
 	properties = gameJson["properties"]
 	for prop in properties:
 		prop["group_id"] = groups.index(prop["group"].lower())
-
 	properties = sorted(properties, key=lambda p: p["position"])
-	
-	return properties, groups
+	return properties
+
+def loadGameCards_CmntyChest(gameJson):
+	#return gameJson["cards"]["community_chest"]
+	return list(range(10))
+
+def loadGameCards_Chance(gameJson):
+	#return gameJson["cards"]["chance"]
+	return list(range(10))
+
+def loadLookup(location):
+	gameJson = loadGameJson(location)
+	properties = loadGameProperties(gameJson)
+	community_chest = loadGameCards_CmntyChest(gameJson)
+	chance = loadGameCards_Chance(gameJson)
+
+	return properties, community_chest, chance
+		
