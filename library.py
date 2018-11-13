863bec7fa3be194e4e539bdc54fcfcd700c7d1e3
James Morris <jamesamorris@yahoo.com>
2018-11-12 20:44:57 -0500

    Fix json.load statement AND sort properties

--- library.py
+++ library.py
@@ -3,11 +3,13 @@ import json
 def loadGameConfigurations(location):
 	gameJson = None
 	with open(location, "r") as fp:
-		gameJson = json.loads(fp)
+		gameJson = json.load(fp)
 
 	groups = gameJson["groups"]
 	properties = gameJson["properties"]
 	for prop in properties:
 		prop["group_id"] = groups.index(prop["group"].lower())
+
+	properties = sorted(properties, key=lambda p: p["position"])
 	
 	return properties, groups
